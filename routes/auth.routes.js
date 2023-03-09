39f5846094df51953a340ff96bb2b98d0f8a0cbc
jdluis <jdluisdev@gmail.com>
2023-03-09 15:32:00 +0000

    MVP: auth funcionality working Ok

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,11 +3,13 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const isAuthenticated = require("../middlewares/auth.middleware");
 const User = require("../models/User.model");
+const Instructor = require("../models/Instructor.model");
+const Student = require("../models/Student.model");
 
 //POST "/api/auth/signup" => Registrar usuario en DB
 router.post("/signup", async (req, res, next) => {
   try {
-    const { email, password } = req.body;
+    const { email, password, type } = req.body;
 
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(password, salt);
@@ -22,11 +24,26 @@ router.post("/signup", async (req, res, next) => {
       res.json({ messageDeveloper: "Inputs should not be empty" });
       return;
     } else {
-      await User.create(dataSigup);
+      const newUser = await User.create(dataSigup);
+
+      // ! For now is a type hidden in the body, change depend of what
+      // ! you click in the preview component. i can pass like a state
+
+      if (type === "instructor") {
+        await Instructor.create({
+          user_id: newUser._id,
+        });
+      } else if (type === "student") {
+        await Student.create({
+          user_id: newUser._id,
+        });
+      } else {
+        res.status(400).json({ errorMessage: "Somethinq was wrong" });
+      }
     }
 
     //test
-    res.json({ errorMessage: "Post of register Ok", data: dataSigup });
+    res.json({ succeesMessage: "Post of register Ok" });
   } catch (err) {
     next(err);
     console.log(err);
@@ -79,7 +96,7 @@ router.post("/login", async (req, res, next) => {
 
 //GET "/api/auth/verify" => User active or not?
 router.get("/verify", isAuthenticated, (req, res, next) => {
-  console.log(req.payload)
+  console.log(req.payload);
   res.status(200).json(req.payload);
 });
 module.exports = router;
