75502eb193a469b35331d7dd9086762a29681aae
jdluis <jdluisdev@gmail.com>
2023-03-15 14:16:30 +0000

    delete console and rebuid back in prod

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -48,7 +48,6 @@ router.post("/signup", async (req, res, next) => {
     res.status(201).json({ succeesMessage: "Post of register Ok" });
   } catch (err) {
     next(err);
-    console.log(err);
   }
 });
 
